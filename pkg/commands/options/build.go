817b1c602c039afcc8184c32bc649765adae8394
Louis Brunner <louis.brunner.fr@gmail.com>
2023-12-04 14:31:36 +0000

    feat: allow commas in --image-label but not multiple labels

--- pkg/commands/options/build.go
+++ pkg/commands/options/build.go
@@ -93,7 +93,7 @@ func AddBuildOptions(cmd *cobra.Command, bo *BuildOptions) {
 		"Path to file where the SBOM will be written.")
 	cmd.Flags().StringSliceVar(&bo.Platforms, "platform", []string{},
 		"Which platform to use when pulling a multi-platform base. Format: all | <os>[/<arch>[/<variant>]][,platform]*")
-	cmd.Flags().StringSliceVar(&bo.Labels, "image-label", []string{},
+	cmd.Flags().StringArrayVar(&bo.Labels, "image-label", []string{},
 		"Which labels (key=value) to add to the image.")
 	cmd.Flags().BoolVar(&bo.Debug, "debug", bo.Debug,
 		"Include Delve debugger into image and wrap around ko-app. This debugger will listen to port 40000.")
